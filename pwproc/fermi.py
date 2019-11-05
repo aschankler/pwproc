da0d003baa57896b34dc7c0869ed4963f80fb09c
Aaron Schankler <ams19@sas.upenn.edu>
2019-11-05 13:30:19 -0500

    expand  interface

--- pwproc/fermi.py
+++ pwproc/fermi.py
@@ -1,10 +1,14 @@
-
-import re
-from pwproc.parsers import get_save_file
-from pwproc.util import parser_one_line
+"""
+Defines the `fermi` subcommand for extracting the fermi energy.
+"""
 
 
 def find_fermi(path):
+    # type: (Path) -> Iterable[float]
+    """Extract all records of the Fermi energy from pw.x output."""
+    import re
+    from pwproc.util import parser_one_line
+
     fermi_re = re.compile(r"[ \t]+the Fermi energy is[ \t]+(-?[.\d]+) ev")
     fermi_parser = parser_one_line(fermi_re, lambda m: float(m.group(1)), find_multiple=True)
 
@@ -14,22 +18,46 @@ def find_fermi(path):
     return fe
 
 
+def write_fermi(outfile, data, write_all=False):
+    # type: (TextIO, Iterable[str, Iterable[float]], bool) -> None
+    """Write fermi energy to data file."""
+    for prefix, fe in data:
+        if write_all:
+            # Write all FE for each file
+            outfile.write(prefix + '\n')
+            for en in fe:
+                outfile.write('{}\n'.format(en))
+        else:
+            # Only write the final FE
+            outfile.write("{}: {}\n".format(prefix, fe[-1]))
+
+
 def parse_args(args):
-    from argparse import ArgumentParser
+    import sys
+    from argparse import ArgumentParser, FileType
 
     parser = ArgumentParser(prog='pwproc fermi')
 
     parser.add_argument('in_file', action='store', nargs='+')
+    parser.add_argument('--out', action='store', type=FileType('w'), default=sys.stdout)
+    parser.add_argument('--all', action='store_true')
 
     return parser.parse_args(args)
+
+
+def fermi(args):
+    # type: (Namespace) -> None
+    from pwproc.parsers import get_save_file
+
+    names = [get_save_file(p) for p in args.in_file]
+    fe = [find_fermi(p) for p in args.in_file]
+
+    write_fermi(args.out, zip(names, fe), write_all=args.all)
+    args.out.close()
 
 
 if __name__ == '__main__':
     import sys
     args = parse_args(sys.argv[1:])
+    fermi(args)
 
-    names = [get_save_file(p) for p in args.in_file]
-    fe = [find_fermi(p)[-1] for p in args.in_file]
-
-    for n, f in zip(names, fe):
-        print("{}: {}".format(n, f))
