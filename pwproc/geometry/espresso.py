c3c0d603c1268e26004565d4775f316b8c85016b
Aaron Schankler <ams19@sas.upenn.edu>
2020-02-19 11:51:20 -0800

    Enable fixing atoms in pwi generation

--- pwproc/geometry/espresso.py
+++ pwproc/geometry/espresso.py
@@ -1,7 +1,7 @@
 """Read/write for Quantum ESPRESSO input files."""
 
 import numpy as np
-from typing import Iterator, Tuple
+from typing import Iterator, Optional, Sequence, Tuple, Union
 
 Species = Tuple[str, ...]
 Basis = np.ndarray      # Shape: (3, 3)
@@ -12,12 +12,15 @@ def read_pwi(lines):
     raise NotImplementedError
 
 
-def gen_pwi(basis, species, pos, coord_type, write_cell=True, write_pos=True):
-    # type: (Basis, Species, Tau, str, bool, bool) -> Iterator[str]
+def gen_pwi(basis: Basis, species: Species, pos: Tau, coord_type: str,
+            write_cell: bool = True, write_pos: bool = True,
+            if_pos: Optional[Sequence[Union[None, Sequence[bool]]]] = None
+           ) -> Iterator[str]:
     """Generate the `CELL_PARAMETERS` and `ATOMIC_POSITIONS` cards.
 
     Basis is assumed to be in angstroms and tau should agree with `coord_type`
     """
+    from itertools import starmap
     from pwproc.geometry.format_util import format_basis, format_tau
 
     # Yield basis
@@ -29,4 +32,17 @@ def gen_pwi(basis, species, pos, coord_type, write_cell=True, write_pos=True):
     # Yield atomic positions
     if write_pos:
         yield "ATOMIC_POSITIONS {}\n".format(coord_type)
-        yield format_tau(species, pos)
+        if if_pos is None:
+            yield format_tau(species, pos)
+        else:
+            # Optionally add the position freeze
+            assert(len(if_pos) == len(species))
+
+            def if_string(ifp):
+                if ifp:
+                    return "   {}  {}  {}".format(*map(lambda b: int(b), ifp))
+                else:
+                    return ""
+
+            pos_lines = format_tau(species, pos).split('\n')
+            yield from starmap(lambda p, ifp: p + if_string(ifp) + '\n', zip(pos_lines, if_pos))
