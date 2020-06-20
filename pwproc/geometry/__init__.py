1d81742d7e59e7d67811a40966c9d44ccdb75f92
Aaron Schankler <ams19@sas.upenn.edu>
2020-06-20 11:39:10 -0400

    Update geometry API

--- pwproc/geometry/__init__.py
+++ pwproc/geometry/__init__.py
@@ -4,6 +4,7 @@
 from pwproc.geometry.data import GeometryData, RelaxData
 
 # Put read/write functions on the namespace
-from pwproc.geometry.espresso import read_pwi, gen_pwi
+from pwproc.geometry.espresso import read_pwi, parse_pwi_atoms, parse_pwi_cell
+from pwproc.geometry.espresso import gen_pwi, gen_pwi_atoms, gen_pwi_cell
 from pwproc.geometry.poscar import read_poscar, gen_poscar
 from pwproc.geometry.xsf import read_xsf, gen_xsf, gen_xsf_animate
